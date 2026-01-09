038c9b52c0819785be0ce847b751f04c9e55a4d2
Nahverkehr <83949072+Nahverkehr@users.noreply.github.com>
2026-01-10 00:22:05 +0100

    V.0.3.0
    Die Definitionen der Schaltflächen wurden in eine neue Datei namens „buttons.js“ verschoben, und die Navigations- und Hero-Schaltflächen werden nun dynamisch in den Dateien „index.html“ und „header.html“ gerendert. Das Logo in der Kopfzeile wurde aktualisiert und verlinkt nun auf die Startseite. Die Stile der Schaltflächen in der Datei „style.css“ wurden verbessert, um eine bessere visuelle Konsistenz und Hervorhebung zu erzielen. Es wurden kleinere Code-Bereinigungen vorgenommen und das Laden von Skripten wurde im Hinblick auf die Wartbarkeit verbessert.

---
+++ buttons.js
@@ -0,0 +1,6 @@
+// buttons.js
+const buttons = [
+  { text: "Bewerben", url: "apply.html", type: "main" },
+  { text: "Discord beitreten", url: "https://discord.com/invite/SePmmzqu42", type: "discord", target: "_blank" },
+  { text: "Unsere VTC-Seite", url: "https://truckersmp.com/vtc/43680", type: "vtc", target: "_blank" }
+];
